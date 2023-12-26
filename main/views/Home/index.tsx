fd314802dfa6a56439ea75607cd7ccb6b52229a8
doubleam <admin@biugle.cn>
2023-12-26 18:27:56 +0800

    Feat: 优化页面布局

--- main/views/Home/index.tsx
+++ main/views/Home/index.tsx
@@ -2,13 +2,13 @@
  * @Author: HxB
  * @Date: 2023-12-21 17:31:18
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-12-26 17:13:58
+ * @LastEditTime: 2023-12-26 17:55:17
  * @Description: 主程序页面
  * @FilePath: \web_mods_base\main\views\Home\index.tsx
  */
 import React, { useEffect, useRef, useState } from 'react';
 import './style.less';
-import { Button, Tabs, Tag, message } from 'antd';
+import { Button, Tabs, message } from 'antd';
 import AdaptiveWebView from '@/components/AdaptWebView';
 import AntIcon from '@/components/AntIcon';
 
@@ -56,78 +56,83 @@ const Home = () => {
         </div>
       </div>
       <div className="main">
-        <div id="tab-container">
+        <div id="tool-container">
           <div
             className="tool-icon-btn"
             onClick={() => {
               document.getElementById('tool-container').classList.toggle('collapsed');
-              document.getElementById('tab-container').classList.toggle('collapsed');
             }}
           >
             <h5 className="tool-title">工具栏</h5>
-            <AntIcon className="tool-open-icon" icon="MenuUnfoldOutlined"></AntIcon>
-            <AntIcon className="tool-close-icon" icon="MenuFoldOutlined"></AntIcon>
-          </div>
-          <div style={{ maxWidth: 'calc(100% - 210px - 128px)' }}>
-            <Tabs
-              hideAdd
-              size="small"
-              onChange={(key) => {
-                setActiveTab(key);
-              }}
-              activeKey={activeTab}
-              type="editable-card"
-              onEdit={(targetKey: string, action: 'add' | 'remove') => {
-                if (action === 'remove') {
-                  window.xIpc.send('close-mods', targetKey);
-                }
-              }}
-              items={tabViews.map((i: any) => ({
-                key: i.name,
-                label: i.displayName,
-              }))}
-            />
-          </div>
-          <div style={{ marginLeft: 'auto', minWidth: '210px', display: 'flex', justifyContent: 'flex-end' }}>
-            <AntIcon
-              className="icon-btn"
-              onClick={() => {
-                window.xIpc.send('change-module-history', activeTab, 'back');
-              }}
-              icon="LeftCircleOutlined"
-            ></AntIcon>
-            <AntIcon
-              className="icon-btn"
-              onClick={() => {
-                window.xIpc.send('change-module-history', activeTab, 'forward');
-              }}
-              icon="RightCircleOutlined"
-            ></AntIcon>
-            <AntIcon
-              className="icon-btn"
-              onClick={() => {
-                window.xIpc.send('close-mods', undefined);
-              }}
-              icon="CloseCircleTwoTone"
-            ></AntIcon>
-            <AntIcon
-              className="icon-btn"
-              onClick={() => {
-                window.xIpc.send('toggle-module-devTools', activeTab);
-              }}
-              icon="ChromeOutlined"
-            ></AntIcon>
-            <AntIcon
-              className="icon-btn reload"
-              onClick={() => {
-                window.xIpc.send('reload-module-page', activeTab);
-              }}
-              icon="SyncOutlined"
-            ></AntIcon>
+            <AntIcon title="展开" className="tool-open-icon" icon="MenuUnfoldOutlined"></AntIcon>
+            <AntIcon title="收起" className="tool-close-icon" icon="MenuFoldOutlined"></AntIcon>
           </div>
         </div>
         <div className="modules-wrapper">
-          <div id="tool-container"></div>
+          <div id="tab-container">
+            <div className="tabs-box">
+              <Tabs
+                hideAdd
+                size="small"
+                onChange={(key) => {
+                  setActiveTab(key);
+                }}
+                activeKey={activeTab}
+                type="editable-card"
+                onEdit={(targetKey: string, action: 'add' | 'remove') => {
+                  if (action === 'remove') {
+                    window.xIpc.send('close-mods', targetKey);
+                  }
+                }}
+                items={tabViews.map((i: any) => ({
+                  key: i.name,
+                  label: i.displayName,
+                }))}
+              />
+            </div>
+            <div className="tabs-options">
+              <AntIcon
+                className="icon-btn"
+                title="后退"
+                onClick={() => {
+                  window.xIpc.send('change-module-history', activeTab, 'back');
+                }}
+                icon="LeftCircleOutlined"
+              ></AntIcon>
+              <AntIcon
+                className="icon-btn"
+                title="前进"
+                onClick={() => {
+                  window.xIpc.send('change-module-history', activeTab, 'forward');
+                }}
+                icon="RightCircleOutlined"
+              ></AntIcon>
+              <AntIcon
+                className="icon-btn"
+                title="关闭所有模块"
+                onClick={() => {
+                  window.xIpc.send('close-mods', undefined);
+                }}
+                icon="CloseCircleTwoTone"
+              ></AntIcon>
+              <AntIcon
+                className="icon-btn"
+                title="打开模块控制台"
+                onClick={() => {
+                  window.xIpc.send('toggle-module-devTools', activeTab);
+                }}
+                icon="ChromeOutlined"
+              ></AntIcon>
+              <AntIcon
+                className="icon-btn reload"
+                title="刷新模块"
+                onClick={() => {
+                  window.xIpc.send('reload-module-page', activeTab);
+                }}
+                icon="SyncOutlined"
+              ></AntIcon>
+            </div>
+          </div>
           <div id="webview-container">
             {/* <AdaptiveWebView src={'https://baidu.com'} /> */}
             {tabViews.map((item) => (

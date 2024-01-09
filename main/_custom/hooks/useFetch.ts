39f39136863f5de1acf26c1cf7d5a4d39b3304e1
doubleam <admin@biugle.cn>
2024-01-09 12:37:28 +0800

    Feat: 调整 useFetch 自动解析

--- main/_custom/hooks/useFetch.ts
+++ main/_custom/hooks/useFetch.ts
@@ -3,7 +3,7 @@
  * @Author: HxB
  * @Date: 2024-01-08 18:21:39
  * @LastEditors: DoubleAm
- * @LastEditTime: 2024-01-09 10:33:45
+ * @LastEditTime: 2024-01-09 12:35:48
  * @Description: useFetch 自定义 hooks
  * @FilePath: \web_mods_base\main\_custom\hooks\useFetch.ts
  */
@@ -13,32 +13,57 @@ const useFetch = (
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH',
   url: string,
   dataOrParams?: any,
-  headers?: any,
+  headers: any = {},
 ) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const parseResponse = useCallback(async (response, contentType) => {
+    if (contentType?.includes('json')) {
+      return await response.json();
+    } else if (contentType?.includes('text')) {
+      return await response.text();
+    } else if (contentType?.includes('form')) {
+      return await response.formData();
+    } else if (contentType?.includes('video') || contentType?.includes('image')) {
+      return await response.blob();
+    } else if (contentType?.includes('arrayBuffer')) {
+      return await response.arrayBuffer();
+    } else {
+      try {
+        if (headers.callback) {
+          return await headers.callback(response);
+        }
+        return response;
+      } catch (e) {
+        return response;
+      }
+    }
+  }, []);
+
   const fetchData = useCallback(async () => {
     setIsLoading(true);
+
+    const contentType =
+      headers.contentType ??
+      headers.ContentType ??
+      headers['content-type'] ??
+      headers['content-Type'] ??
+      headers?.['Content-Type'];
 
     try {
       const options: any = {
         method,
+        // 文件请求相关处理时需注意别写 content-type
         headers: {
-          ...(headers ?? {}),
-          // 文件请求相关处理时需注意别写 content-type
-          ...(!headers || headers?.isFile
+          ...headers,
+          callback: undefined, // 去除非法参数
+          ...(!contentType || headers.isFile
             ? {}
             : {
-                'content-type':
-                  headers?.contentType ??
-                  headers?.ContentType ??
-                  headers['Content-Type'] ??
-                  headers['content-type'] ??
-                  headers['content-Type'] ??
-                  'application/x-www-form-urlencoded;charset=UTF-8',
-                // 'application/json;charset=UTF-8',
+                'content-type': contentType ?? 'application/x-www-form-urlencoded;charset=UTF-8',
+                // ?? 'application/json;charset=UTF-8',
               }),
         },
       };
@@ -54,25 +79,26 @@ const useFetch = (
       }
 
       const response = await fetch(newUrl, options);
-      const json = await response.json();
+      const type = response.headers.get('content-type');
+      const responseData = await parseResponse(response, type);
 
       if (response.ok) {
-        setData(json);
+        setData(responseData);
       } else {
-        setError(json);
+        setError(responseData);
       }
     } catch (error) {
       setError(error);
     } finally {
       setIsLoading(false);
     }
-  }, [method, url, dataOrParams, headers]);
+  }, [parseResponse]);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  return { data, error, isLoading };
+  return [data, error, isLoading];
 };
 
 export default useFetch;
